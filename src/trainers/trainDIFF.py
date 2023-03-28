df713c41a558eaa2d0338019dab592c3d5a25ca4
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-03-28 16:14:10 +0800

    changed preview settings

--- src/trainers/trainDIFF.py
+++ src/trainers/trainDIFF.py
@@ -7,7 +7,7 @@ from src.visualization.tools import preview_images
 
 def train(model, device, training_dataset, optimizer, loss_function, times, beta_zero, beta_end):
 
-    plot_every_batch = 25
+    plot_every_batch = 10
     plot_every_time = 100
 
     timesteps = torch.from_numpy(np.arange(0, times)).view(-1, 1).to(device, dtype=torch.int64)
