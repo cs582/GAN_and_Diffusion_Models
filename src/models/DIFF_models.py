fe70e16b1ab5e22654c50433d78517f43e5d0099
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-03-28 09:15:16 +0800

    added logs to MNISTDiffusion

--- src/models/DIFF_models.py
+++ src/models/DIFF_models.py
@@ -22,8 +22,9 @@ class MNISTDiffusion(nn.Module):
         time_token = time_token.view(x.shape[0], *x[0].shape)
 
         print(f"x shape {x.shape} token shape {time_token.shape}")
+        x = x * time_token
 
-        x = self.dense(x * time_token)
+        x = self.dense(x)
 
         x = self.block1(x)
         x = self.block2(x)
