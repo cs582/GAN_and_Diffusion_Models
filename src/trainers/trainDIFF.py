bcd149333d0277c855c8e5fd67ed0c75c8661aa1
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-03-28 17:04:32 +0800

    fixed training loop

--- src/trainers/trainDIFF.py
+++ src/trainers/trainDIFF.py
@@ -13,27 +13,29 @@ def train(model, device, training_dataset, optimizer, loss_function, times, beta
     timesteps = torch.from_numpy(np.arange(0, times)).view(-1, 1).to(device, dtype=torch.int64)
     beta_range = torch.from_numpy(np.linspace(beta_zero, beta_end, times)).to(device)
 
-    #for batch_n, (x, _) in enumerate(training_dataset):
-    for i in range(0, times):
-        t = times - timesteps[i] - 1
+    prev_images_memory = [x for x, y in training_dataset]
+
+    n_batches = len(training_dataset)
+
+    for i in range(0, times-1):
+        t = timesteps[i] + 1
 
         print(f"Training T {t.item()}...")
         beta = beta_range[i]
 
         loss_history = []
 
-        #for i in tqdm(range(0, times), total=times):
-        for batch_n, (x, _) in tqdm(enumerate(training_dataset), total=len(training_dataset)):
+        for batch_n in tqdm(range(0, n_batches), total=n_batches):
 
-            prev_imgs = x.to(device)
+            # Retrieve previous images (t-1) from memory
+            prev_imgs = prev_images_memory[batch_n].to(device)
 
-            if i == 0 and batch_n % plot_every_batch == 0:
-                preview_images(prev_imgs, 5, 5, "preview/MNIST_DIFF", f"batch_{batch_n}_T_{t.item()+1}")
-
+            # Add noice to previous images (t-0) to get current images (t)
             curr_imgs = noise_images(prev_imgs, beta)
 
             optimizer.zero_grad()
 
+            # Reconstruct current image to recover previous image
             imgs_reconstructed = model(curr_imgs, t)
 
             loss = loss_function(imgs_reconstructed, prev_imgs)
@@ -43,8 +45,14 @@ def train(model, device, training_dataset, optimizer, loss_function, times, beta
 
             loss_history.append(loss.item())
 
+            # Print original image
+            if i == 0 and batch_n % plot_every_batch == 0:
+                preview_images(prev_imgs, 5, 5, "preview/MNIST_DIFF", f"batch{batch_n}_T{t.item()-1}")
+
+            # Print reconstruction every interval of batches and interval of time
             if t % plot_every_time == 0 and batch_n % plot_every_batch == 0:
-                preview_images(imgs_reconstructed, 5, 5, "preview/MNIST_DIFF", f"batch_{batch_n}_T_{t.item()}")
-                prev_imgs = curr_imgs
+                preview_images(imgs_reconstructed, 5, 5, "preview/MNIST_DIFF", f"batch{batch_n}_T{t.item()}")
+
+            prev_images_memory[batch_n] = curr_imgs
 
         print(f"AVG LOSS: {np.round(np.mean(loss_history), 3)}")
