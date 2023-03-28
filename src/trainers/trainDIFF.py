2bcff92e636b4b4792298e9801d406217e7b0ba7
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-03-28 13:34:14 +0800

    fixed training loop

--- src/trainers/trainDIFF.py
+++ src/trainers/trainDIFF.py
@@ -6,28 +6,23 @@ from src.data_utils.transformations import noise_images
 
 def train(model, device, training_dataset, optimizer, loss_function, times, beta_zero, beta_end):
 
-    timesteps = torch.from_numpy(np.arange(0, times)).to(device)
+    timesteps = torch.from_numpy(np.arange(0, times)).view(-1, 1).to(device, dtype=torch.int64)
     beta_range = torch.from_numpy(np.linspace(beta_zero, beta_end, times)).to(device)
 
     for i in range(0, times):
         print(f"Training Timestep {times-i}...")
 
-        t = times - timesteps[i]
+        t = times - timesteps[i] - 1
         beta = beta_range[i]
 
         loss_history = []
 
         for j, (x, _) in tqdm(enumerate(training_dataset), total=len(training_dataset)):
             if j == 0:
-                prev_imgs = 2*x.to(device) - 1.0
+                prev_imgs = x.to(device)
                 continue
 
             curr_imgs = noise_images(prev_imgs, beta)
-
-            print(f"batch number (j) = {j}")
-            print(f"curr_imgs type {type(curr_imgs)} and size {curr_imgs.shape}")
-            print(f"beta value = {beta}")
-            print("min", curr_imgs.min(), "max", curr_imgs.max())
 
             optimizer.zero_grad()
 
@@ -41,5 +36,7 @@ def train(model, device, training_dataset, optimizer, loss_function, times, beta
             loss_history.append(loss.item())
 
             prev_imgs = curr_imgs
+
+            print(f"finished taking step {j}")
 
         print(f"AVG LOSS: {np.round(np.mean(loss_history), 3)}")
