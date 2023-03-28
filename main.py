82ca39194625fa8fbe1d74a7023caa78b16acf2b
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-03-28 09:28:48 +0800

    fixed batch_size argument

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-epoch', type=int, default=10, help="Number of epochs.")
 parser.add_argument('-timesteps', type=int, default=10, help="Number of timesteps.")
-parser.add_argument('-bach_size', type=int, default=64, help="Batch size.")
+parser.add_argument('-batch_size', type=int, default=64, help="Batch size.")
 parser.add_argument('-lr', type=float, default=1e-4, help="Learning Rate.")
 parser.add_argument('-latent_vs', type=int, default=100, help="Latent vector size.")
 parser.add_argument('-model', type=str, default="DIFF", help="Choose model to train.")
@@ -22,11 +22,11 @@ if __name__ == "__main__":
 
     if args.model == "GAN":
         if args.dataset == "MNIST":
-            GAN_MNIST.run(epochs=args.epochs, batch_size=args.bach_size, latent_vector_size=args.latent_vs, lr=args.lr)
+            GAN_MNIST.run(epochs=args.epochs, batch_size=args.batch_size, latent_vector_size=args.latent_vs, lr=args.lr)
 
     if args.model == "DIFF":
         if args.dataset == "MNIST":
-            DIFF_MNIST.run(batch_size=args.bach_size, timesteps=args.timesteps, lr=args.lr)
+            DIFF_MNIST.run(batch_size=args.batch_size, timesteps=args.timesteps, lr=args.lr)
 
 
 
